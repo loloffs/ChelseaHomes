d4eb48202c689835508fa81155c4ac1df4734d5b
loloffs <lukeoloffs@gmail.com>
2021-05-18 15:31:36 -0700

    Server and package json

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
+// const path = require('path');
+// const router = express.Router();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/index.html'));
-  //__dirname : It will resolve to your project folder.
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.get('/index', (req, res) => {
+  res.render('index.html');
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
