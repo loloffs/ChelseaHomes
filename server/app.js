7245add91d75371f4265b58555115febd99204a7
loloffs <lukeoloffs@gmail.com>
2021-05-18 15:13:55 -0700

    Express server setup

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = express.Router();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.render('./index')
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
