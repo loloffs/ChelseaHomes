fb922f3f86017cc97826d2d8b1c3e217fd68ee0f
loloffs <lukeoloffs@gmail.com>
2021-05-19 11:52:36 -0700

    Routes and partials

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const expressLayouts = require('express-ejs-layouts');
 
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
 
+app.use(expressLayouts)
 app.set('view engine', 'ejs')
 
 app.get('', (req, res) => {
   res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/gallery', (req, res) => {
+  res.render('gallery');
+});
+
+app.get('/quote', (req, res) => {
+  res.render('quote');
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
