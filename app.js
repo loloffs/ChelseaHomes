3c1db8c0408f664197045956058452c49b281c44
loloffs <lukeoloffs@gmail.com>
2021-05-19 16:20:09 -0700

    Starting quote form and footer partial

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-// const expressLayouts = require('express-ejs-layouts');
 
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
-
-// app.use(expressLayouts)
-// app.set('layout', './layouts/_header')
 app.set('view engine', 'ejs')
 
 app.get('', (req, res) => {
