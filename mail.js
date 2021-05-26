290a9efc08d8ce26f14d5d266a970efc13a33568
loloffs <lukeoloffs@gmail.com>
2021-05-26 12:01:19 -0700

    Mail.js

--- mail.js
+++ mail.js
@@ -3,7 +3,7 @@ const mailGun = require('nodemailer-mailgun-transport');
 
 const auth = {
   auth: {
-    api_key: '3a6e091fa71ede60a02e9ebb18c41e14-fa6e84b7-604d17eb',
+    api_key: '',
     domain: 'sandboxd5ff4e8d7df74c21984c7f7af1e22cca.mailgun.org'
   }
 };
